e2fb5b1b0e306e79555b4d92fac7c52648e7007d
justinmrks <justinmarks19@gmail.com>
2022-04-15 01:39:06 -0500

    put cards into grids and added image support

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 const CardList = (props) => {
   return (
-    <div>
+    <div className="cardHolder">
       {props.error ? (
         <div> Uhh Ohh {props.error}</div>
       ) : (
         props.cards.map((card, key) => (
-          <div>
+          <div className="singleCard">
+            {card.imageUrl ? (
+              <img src={card.imageUrl} alt={`Card art for ${card.name}`} />
+            ) : (
+              <p>No Image found for {card.name}</p>
+            )}
             <h2>{card.name}</h2>
+            <h3>{card.type}</h3>
+            <p>{card.text}</p>
           </div>
         ))
       )}
